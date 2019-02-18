87f7592fc2d7464eba00ac9a24af55ec3d3d7892
mson90906250 <l90906250@gmail.com>
2019-02-18 08:23:53 +0800

    I_culture_shop_ver2 只更改product的内容

---
+++ I_culture_shop_ver2/src/main/java/tw/tcnr01/I_culture/ShopFragment.java
@@ -0,0 +1,79 @@
+package tw.tcnr01.I_culture;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ShopFragment extends Fragment implements View.OnClickListener {
+    private ImageView img01,img02;
+    private Intent intent=new Intent();
+
+    private FragmentTransaction mTrasaction;
+    private Shop02Fragment shop02Fragment ;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //系統原生用不到
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.shop_fragment,container,false);
+        img01=v.findViewById(R.id.shop01_img01);
+        img02=v.findViewById(R.id.shop01_img02);
+
+        img01.setOnClickListener(this);
+        img02.setOnClickListener(this);
+
+
+        return v;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.shop01_img01:
+
+                //用fragment的做法
+                shop02Fragment = new Shop02Fragment();
+                mTrasaction = getActivity().getSupportFragmentManager().beginTransaction();
+                Bundle bundle = new Bundle();
+                bundle.putString("class_title",getString(R.string.app_name));
+                bundle.putString("Kind","f");
+                //fragment傳值
+                shop02Fragment.setArguments(bundle);
+                mTrasaction.replace(R.id.container,shop02Fragment).commit();
+
+                //由於改為fragment intent的方法就不能用了
+                /*intent.putExtra("class_title", getString(R.string.app_name));
+               intent.setClass(shop01.this, shop02.class);
+                intent.putExtra("Kind", "f");*/
+                break;
+            case R.id.shop01_img02:
+
+                //用fragment的做法
+                shop02Fragment = new Shop02Fragment();
+                mTrasaction = getActivity().getSupportFragmentManager().beginTransaction();
+                bundle = new Bundle();
+                bundle.putString("class_title",getString(R.string.app_name));
+                bundle.putString("Kind","c");
+                //fragment傳值
+                shop02Fragment.setArguments(bundle);
+                mTrasaction.replace(R.id.container,shop02Fragment).commit();
+
+               /* intent.putExtra("class_title", getString(R.string.app_name));
+                intent.setClass(shop01.this, shop02.class);
+                intent.putExtra("Kind", "c");*/
+                break;
+        }
+       // startActivity(intent);
+    }
+}
