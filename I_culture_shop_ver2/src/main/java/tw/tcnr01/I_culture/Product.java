909857fc85c6408a9825bc443821250e9469b217
mson90906250 <l90906250@gmail.com>
2019-02-19 08:27:19 +0800

    修改Product

--- I_culture_shop_ver2/src/main/java/tw/tcnr01/I_culture/Product.java
+++ I_culture_shop_ver2/src/main/java/tw/tcnr01/I_culture/Product.java
@@ -3,12 +3,15 @@ package tw.tcnr01.I_culture;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -22,7 +25,9 @@ public class Product extends AppCompatActivity {
     private String sqlctl;
     private String shop;
     private String TAG="tcnr21=>";
-    private TextView tx6   ,tx9;
+    private ImageView pd;
+    private LinearLayout mlay;
+
     private String uri = "https://tcnr1621.000webhostapp.com/I_culture/shop/ANDROIDTEST.php";
 
     @Override
@@ -33,11 +38,32 @@ public class Product extends AppCompatActivity {
         Intent it =getIntent();
         shop=it.getStringExtra("shop_id");
 
+        //-------------抓取遠端資料庫設定執行續------------------------------
+        StrictMode.setThreadPolicy(new
+                StrictMode.
+                        ThreadPolicy.Builder().
+                detectDiskReads().
+                detectDiskWrites().
+                detectNetwork().
+                penaltyLog().
+                build());
+        StrictMode.setVmPolicy(
+                new
+                        StrictMode.
+                                VmPolicy.
+                                Builder().
+                        detectLeakedSqlLiteObjects().
+                        penaltyLog().
+                        penaltyDeath().
+                        build());
+//---------------------------------------------------------------------
+
         sqlctl = "SELECT * FROM shop_product WHERE Shop_ID=\""+shop+"\"  ORDER BY Shop_ID ASC";
 
 
-        tx6=(TextView)findViewById(R.id.textView6);
-        tx9=(TextView)findViewById(R.id.textView9);
+
+
+
 
 
         setupViewComponent();
@@ -45,7 +71,10 @@ public class Product extends AppCompatActivity {
 
     private void setupViewComponent() {
 
-    img01=(ImageView)findViewById(R.id.product_img01);
+//        ScrollView scr01=(ScrollView)findViewById(R.id.scr01) ;
+        mlay=(LinearLayout)findViewById(R.id.product_lay01);
+        ImageView img01=(ImageView)findViewById(R.id.product_img01);
+        img01.setVisibility(View.GONE);
 
         try {
             String result = DBConnector.executeQuery(sqlctl,uri);
@@ -58,17 +87,23 @@ public class Product extends AppCompatActivity {
             // ---
             Log.d(TAG, jsonArray.toString());
             // --
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonData = jsonArray.getJSONObject(i);
                 // // 取出 jsonObject 中的字段的值的空格
                 Iterator itt = jsonData.keys();
 
+                pd=new ImageView(this);
+                pd.setId(i);
+
+                pd.setImageResource(R.drawable.product_img);
+                mlay.addView(pd);
+
                 final String Name=jsonData.getString("Product_name");
                 final String Introduce=jsonData.getString("Product_msg");
-                String URL =jsonData.getString("URL");
+//                String URL =jsonData.getString("URL");
 
 //            設置產品
-                img01.setOnClickListener(new View.OnClickListener() {
+                pd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         productDlg = new Dialog(Product.this);
@@ -76,6 +111,10 @@ public class Product extends AppCompatActivity {
                         productDlg .setContentView(R.layout.product_dialog);
                         ImageButton btncancel=(ImageButton)productDlg.findViewById(R.id.product_dialog_btncancel);
                         btncancel.setOnClickListener(productbtncancel);
+
+                        TextView tx6=(TextView)productDlg.findViewById(R.id.textView6);
+                        TextView tx9=(TextView)productDlg.findViewById(R.id.textView9);
+
                         tx6.setText(Name);
                         tx9.setText(Introduce);
                         productDlg .show();
@@ -110,46 +149,5 @@ public class Product extends AppCompatActivity {
         }
 
     }
-
-
-
-    private void Mysqlsel(String sqlctl) {
-        try {
-            String result = DBConnector.executeQuery(sqlctl,uri);
-            /**************************************************************************
-             * SQL 結果有多筆資料時使用JSONArray
-             * 只有一筆資料時直接建立JSONObject物件 JSONObject
-             * jsonData = new JSONObject(result);
-             **************************************************************************/
-            JSONArray jsonArray = new JSONArray(result);
-            // ---
-            Log.d(TAG, jsonArray.toString());
-            // --
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonData = jsonArray.getJSONObject(i);
-                // // 取出 jsonObject 中的字段的值的空格
-                Iterator itt = jsonData.keys();
-//				tr.setGravity(Gravity.CENTER_HORIZONTAL);
-                while (itt.hasNext()) {
-                    String key = itt.next().toString();
-                    String value = jsonData.getString(key);
-                    if (value == null) {
-                        continue;
-                    } else if ("".equals(value.trim())) {
-                        continue;
-                    } else {
-                        jsonData.put(key, value.trim());
-                    }
-                    // --
-                    Log.d(TAG, "i:" + i + " key:" + key + " value:" + value);
-                    // --
-                }
-            }
-        } catch (Exception e) {
-            // Log.d(TAG, e.toString());
-        }
-    }
-
-
 
 }
